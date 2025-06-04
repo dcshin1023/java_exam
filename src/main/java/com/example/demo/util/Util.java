45bcd98c471795de1944b11a426fdeb0803cf326
dcshin1023 <dcshin1023@icloud.com>
2025-06-04 12:24:46 +0900

    Apply sha256 for DB

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -1,5 +1,8 @@
 package com.example.demo.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Util {
 	public static String jsReplace(String msg, String uri) {
 		
@@ -16,11 +19,36 @@ public class Util {
 						const msg = '%s'.trim();
 						
 						if (msg.length > 0) {
-							alert(msg);
+							requestAnimationFrame(() => {
+								alert(msg);
+							})
 						}
 						
-						location.replace('%s');
+						setTimeout(() => {
+							location.replace('%s');
+						}, 100);
 					</script>
 				""", msg, uri);
 	}
+	
+	public static String encryptSHA256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            
+            byte[] hash = digest.digest(input.getBytes());
+            
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 알고리즘을 지원하지 않습니다.", e);
+        }
+    }
 }
